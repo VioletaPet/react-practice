113049e259229e16f3f9df4edfed34f296d4d488
Violeta Petkovic <v.petkovic@protonmail.com>
2025-02-17 16:01:57 +0100

    finishes up home section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import { LoadingScreen } from './components/LoadingScreen';
 import { Navbar } from './components/Navbar';
 import { MobileMenu } from './components/MobileMenu';
+import { Home } from './components/sections/Home';
 
 function App() {
   // code to clear up text once it's done loading
@@ -26,7 +27,7 @@ function App() {
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
 
-
+        <Home />
       </div>
     </>
   );
